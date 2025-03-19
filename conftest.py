23c8100c5b854bb4072567b423de9e2178404424
Sunil Jeurkar <suniljeurkar@gmail.com>
2025-03-19 13:02:24 +0530

    0.1v

--- conftest.py
+++ conftest.py
@@ -9,11 +9,13 @@ def snowflake_conn():
     account = os.getenv("SNOWFLAKE_ACCOUNT")  # HidingSensitive Data
     user = os.getenv("SNOWFLAKE_USER")
     private_key_path = os.getenv("SNOWFLAKE_PRIVATE_KEY_PATH")
+    if not all([account, user, private_key_path]):
+        raise ValueError("Missing environment variables for Snowflake connection!")
 
     with open(private_key_path, "rb") as key_file:
         private_key = serialization.load_pem_private_key(
-        key_file.read(),
-        password=None,  # No password since we removed it
+            key_file.read(),
+            password=None,  # No password since we removed it
     )
 # Connect to Snowflake
     conn = snowflake.connector.connect(
