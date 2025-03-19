a0df7bf0b3500f93fdb37159974f03811b7026cf
Sunil Jeurkar <suniljeurkar@gmail.com>
2025-03-19 12:31:50 +0530

    0.1v

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+import snowflake.connector
+from cryptography.hazmat.primitives import serialization
+import os
+
+@pytest.fixture(scope="module")
+
+def snowflake_conn():
+    account = os.getenv("SNOWFLAKE_ACCOUNT"),  # HidingSensitive Data
+    user = os.getenv("SNOWFLAKE_USER"),
+    private_key_path = os.getenv("SNOWFLAKE_PRIVATE_KEY_PATH"),
+
+    with open(private_key_path, "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,  # No password since we removed it
+    )
+# Connect to Snowflake
+    conn = snowflake.connector.connect(
+        account=account, #HidingSensitive Data
+        user=user,
+        private_key=private_key,
+        role="ACCOUNTADMIN"
+    )
+    yield conn
+    conn.close()
